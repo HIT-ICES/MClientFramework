68c5f4285bff784b83d3de327139bf61cdf36ff4
SeptemberHX <september_hx@outlook.com>
2021-01-14 15:55:43 +0800

    add: MFuncDescription fix: maven pom install error

---
+++ src/main/java/com/hitices/mclient/annotation/MFuncDescription.java
@@ -0,0 +1,16 @@
+package com.hitices.mclient.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by Lei on 2019/12/28 17:07
+ * @author Lei
+ */
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MFuncDescription {
+    String value() default "";
+    int level() default 1;
+}
